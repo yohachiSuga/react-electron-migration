3bd458f5634e0d3143af13f49da976578320f163
sakira <yohachi.suga@gmail.com>
2019-12-27 15:28:17 +0900

    first commit

---
+++ public/index.js
@@ -0,0 +1,26 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL("http://localhost:3000");
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
